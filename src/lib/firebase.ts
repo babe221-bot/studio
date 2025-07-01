2cbe2a8a32d46a76cec1d7da2a660c3c5348f543
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-01 06:42:11 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC90E9Jq1vp_cWfHUMsHjFLS1JDJQBTO8c",
+  authDomain: "stone-c4507.firebaseapp.com",
+  projectId: "stone-c4507",
+  storageBucket: "stone-c4507.appspot.com",
+  messagingSenderId: "334059655965",
+  appId: "1:334059655965:web:4b802e014759c637f887f4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
