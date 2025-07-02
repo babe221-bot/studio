7f18a4584fce08401fa8f6de3bf61ebda819e320
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-02 00:31:12 +0000

    Fasadna ploča isto postavi defoult kao podne ploce samo izmjeni dimenzij

--- src/lib/constructionElements.ts
+++ src/lib/constructionElements.ts
@@ -7,5 +7,5 @@ export const constructionElements: ConstructionElement[] = [
   { id: 'stepenica-celo', name: 'Stepenica - Čelo', defaultLength: 100, defaultWidth: 15, defaultHeight: 2 },
   { id: 'poklopnica-zida', name: 'Poklopnica zida', defaultLength: 100, defaultWidth: 30, defaultHeight: 3 },
   { id: 'podna-ploca', name: 'Podna ploča', defaultLength: 60, defaultWidth: 30, defaultHeight: 2, hasQuantityInput: true },
-  { id: 'fasadna-ploca', name: 'Fasadna ploča', defaultLength: 80, defaultWidth: 40, defaultHeight: 2 },
+  { id: 'fasadna-ploca', name: 'Fasadna ploča', defaultLength: 40, defaultWidth: 20, defaultHeight: 3, hasQuantityInput: true },
 ];
