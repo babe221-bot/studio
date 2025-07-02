c87711cd2a4294d00955e5c578dbb13ad76524ef
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-02 01:21:33 +0000

    poklopnica zida ima obce na komad i na metre duzne

--- src/lib/constructionElements.ts
+++ src/lib/constructionElements.ts
@@ -5,11 +5,11 @@ export const constructionElements: ConstructionElement[] = [
   { id: 'stepenica-gaziste', name: 'Stepenica - Gazište', defaultLength: 100, defaultWidth: 33, defaultHeight: 3, orderUnit: 'piece' },
   { id: 'stepenica-celo', name: 'Stepenica - Čelo', defaultLength: 100, defaultWidth: 15, defaultHeight: 2, orderUnit: 'piece' },
   { id: 'stepenica-cokl', name: 'Stepenica - Cokl', defaultLength: 100, defaultWidth: 10, defaultHeight: 2, orderUnit: 'lm' },
-  { id: 'poklopnica-zida', name: 'Poklopnica zida', defaultLength: 100, defaultWidth: 30, defaultHeight: 3, orderUnit: 'piece' },
+  { id: 'poklopnica-zida', name: 'Poklopnica zida (komad)', defaultLength: 100, defaultWidth: 30, defaultHeight: 3, orderUnit: 'piece' },
+  { id: 'poklopnica-zida-lm', name: 'Poklopnica zida (m)', defaultLength: 100, defaultWidth: 30, defaultHeight: 3, orderUnit: 'lm' },
   { id: 'podna-ploca', name: 'Podna ploča', defaultLength: 60, defaultWidth: 30, defaultHeight: 2, orderUnit: 'sqm' },
   { id: 'fasadna-ploca', name: 'Fasadna ploča', defaultLength: 40, defaultWidth: 20, defaultHeight: 3, orderUnit: 'sqm' },
   { id: 'bunja', name: 'Bunja', defaultLength: 100, defaultWidth: 15, defaultHeight: 5, orderUnit: 'sqm', hasSpecialBunjaEdges: true },
   { id: 'obloga-kamina', name: 'Obloga kamina', defaultLength: 100, defaultWidth: 80, defaultHeight: 2, orderUnit: 'piece'},
-  { id: 'kuhinjska-radna-ploca', name: 'Kuhinjska radna ploča', defaultLength: 220, defaultWidth: 62, defaultHeight: 3, orderUnit: 'piece' },
   { id: 'rubni-kamen-bazena', name: 'Rubni kamen bazena', defaultLength: 60, defaultWidth: 33, defaultHeight: 4, orderUnit: 'lm' },
 ];
